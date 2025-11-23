f12933a23ce23e5a5bbcc802195e4b01e67b9a4b
Madhurya Pasan Perera <90153718+MadhuryaPasan@users.noreply.github.com>
2025-11-23 19:20:18 +0530

    Add hello.ipynb and update hello.py with input handling
    Added a new Jupyter notebook (hello.ipynb) with Python basics, including input, arithmetic, functions, and formatting. Updated hello.py to include improved input handling, string formatting, and commented examples for user interaction.

--- cs50/python/hello.py
+++ cs50/python/hello.py
@@ -1,3 +1,48 @@
-#say hello to user
+# # Ask user for their name
+# name = input("what's your name? ")
 
-print("hello","frined")
+
+# # # say hello to user
+# # print("-----------------------")
+# # print("hello, "+ name)
+# # print("hello,", name)
+
+
+# # # options in print function
+# # print("-----------------------")
+# # print("hello, ", end="")
+# # print(name)
+# # print("-----------------------")
+# # print("hello, ", end="\n")
+# # print(name)
+# # print("-----------------------")
+# # print("hello, ", end="???")
+# # print(name)
+# # print("-----------------------")
+# # print("-----------------------")
+# # print("hello,",name,sep=' ')
+# # print("hello,",name,sep='???')
+
+
+
+# # #say hello to user
+# # print("hello,\"frined") #escape caracters
+
+
+# # #remove witespace from str
+# # name=name.strip()
+# # # capitalize user's input
+# # # name=name.capitalize() # first word only
+# # name = name.title()
+
+
+# # remove witespace from str and capiltalize user's name (adding two thing in one line)
+# name = name.strip().title() # left to right
+# print(f"hello,{name}")
+
+# add witespace from str and capitalize user's name to the input it self to make it more easy
+name = input("what is your name?").strip().title()
+
+print(f"hello {name}")
+
+
